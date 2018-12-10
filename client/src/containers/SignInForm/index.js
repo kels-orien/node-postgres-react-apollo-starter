1e71b312ce24a8be9e7734ad4806f040b5557eac
kels-orien <kelvinorien@gmail.com>
2018-12-10 21:10:22 +0100

    signInform edit

--- client/src/containers/SignInForm/index.js
+++ client/src/containers/SignInForm/index.js
@@ -10,30 +10,137 @@ const INITIALSTATE = {
   confirmPassword: ""
 };
 
-class SignInForm extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      ...INITIALSTATE
-    };
+class SignInForm extends React.Component {
+
+  constructor(props){
+      super();
+      this.state = {
+          ...INITIALSTATE
+      }
   }
+
   clearState() {
-    this.setState({ ...INITIALSTATE });
+      this.setState({...INITIALSTATE})
   }
+
   handleChange(event) {
-    const name = event.target.name;
-    const value = event.target.value;
-    this.setState({
-      [name]: value
-    });
+      const name = event.target.name;
+      const value = event.target.value;
+      this.setState({
+          [name]: value
+      });
   }
-  render() {
-    return (
-      <div>
-        <h1>SignInForm</h1>
-      </div>
-    );
+
+  handleSubmit(event, signinUser) {
+      event.preventDefault();
+      signinUser().then(async ({data}) => {
+          Cookies.set('token', data.signinUser.token);
+          await this.props.refetch();
+          this.clearState();
+          this.props.history.push('/dashboard');
+
+      }).catch(error => {
+          this.setState({
+              error: error.graphQLErrors.map(x => x.message)
+          })
+          console.error("ERR =>", error.graphQLErrors.map(x => x.message));
+      });
+  }
+
+  validate() {
+      const { email, password } = this.state
+      this.state;
+      const isInvalid = !email || !password;
+      return isInvalid;
+  }
+
+  head() {
+      return (
+          <Helmet bodyAttributes={{class: "logInPage"}}>
+              <title>LogIn - React Starter Kit</title>
+          </Helmet>
+      );
+  }
+
+  render(){
+
+      const { email, password } = this.state
+      this.state;
+      
+      return (
+          <div className="column column_12_12">
+              {this.head()}
+              <div className="signUp authForm">
+
+                  <h1 className="dark_headline">
+                      LogIn
+                  </h1>
+
+                  <Mutation mutation={SIGNIN_USER} variables={{ email, password }}>
+
+                      {(signinUser, { data, loading, error }) => {
+
+                          return (
+
+                              <form className="form" onSubmit={event => this.handleSubmit(event, signinUser)}>
+
+                                  <div className="form_wrap">
+
+                                      <div className={classNames({'error-label' : this.state.error != ''})}>
+                                          {this.state.error}
+                                      </div>
+
+                                      <div className="form_row">
+
+                                          <div className="form_item">
+                                              <div className="form_input">
+                                                  <input type="email" name="email" placeholder="Email" value={email} onChange={this.handleChange.bind(this)} />
+                                                  <span className="bottom_border"></span>
+                                              </div>
+                                          </div>
+
+                                      </div>
+
+                                      <div className="form_row">
+
+                                          <div className="form_item">
+                                              <div className="form_input">
+                                                  <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange.bind(this)} />
+                                                  <span className="bottom_border"></span>
+                                              </div>
+                                          </div>
+
+                                      </div>
+
+                                      <div className="formBottomLinks">
+                                          <p>
+                                              Don't have an account? <NavLink to="/signup">Join now!</NavLink>
+                                          </p>
+                                          <p>
+                                              Forgot your password? <NavLink to="/account-recovery">Reset here</NavLink>
+                                          </p>
+                                      </div>
+                                  
+                                      <div className="form_buttons">
+                                          <button type="submit" className="btn"
+                                          disabled={ loading || this.validate() }>
+                                          LogIn</button>
+                                      </div>
+                                  
+                                  </div>
+
+                              </form> 
+
+                          );
+                      }}
+                      
+                  </Mutation>
+
+              </div>
+          </div>
+      )
   }
 }
 
-export default SignInForm;
+export default withRouter(Signin);
+
